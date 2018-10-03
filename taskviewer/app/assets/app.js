285483659233e2f2d362459ba8b8ecff7067c72f
Isuru Edirisinghe <isuruedirisinghe80@gmail.com>
2018-10-03 09:10:55 +0530

    added cross platform support

--- taskviewer/app/assets/app.js
+++ taskviewer/app/assets/app.js
@@ -1,7 +1,12 @@
-//THIS EXAMPLE ONLY WORKS ON WINDOWS PLATFORM 
 let myapp = {
     loadProcesses : () => {
-	Neutralino.os.runCommand('tasklist /FI "STATUS eq RUNNING"', 
+	let key = NL_OS == 'Windows' ? 'tasklist /FI "STATUS eq RUNNING"' : 'ps aux --sort -rss'; //check os and set command
+	let pn = NL_OS == 'Windows' ? 0 : 10; //set index of process name based on OS
+	//Process ID is the same index in both Operating Systems
+	let mUse = NL_OS == 'Windows' ? 4 : 3; //memory usage index based on OS
+	let mUnit = NL_OS == 'Windows' ? 'KB' : '%';
+
+	Neutralino.os.runCommand(key, 
 		(data) => {
 			document.getElementById('name').innerText=data.stdout; //this formats the data into html ,omitting any aditional whitespaces between the process data retrieved
 			var result=document.getElementById('name').innerText.split(/\n/);//split process rows
@@ -11,7 +16,7 @@ let myapp = {
 			for(var i=3;i<result.length-1;i++) { //ommit the table header produced by CMD
 				out+="<tr>";
 				var col=result[i].split(" ");
-				out +="<td>"+col[0]+"</td>"+"<td>"+col[1]+"</td>"+"<td>"+col[4]+" KB</td>"; 
+				out +="<td>"+col[pn]+"</td>"+"<td>"+col[1]+"</td>"+"<td>"+col[mUse]+" "+mUnit+"</td>"; 
 				out +="</tr>"; 
 			};
 
@@ -32,7 +37,7 @@ let myapp = {
 Neutralino.init({
     load: () => {
         myapp.loadProcesses();
-		setInterval(() => {myapp.loadProcesses();},500); //refresh every 500 milliseconds
+		setInterval(() => {myapp.loadProcesses();},5000); //refresh every 5 seconds
     },
     pingSuccessCallback : () => {
         console.log("ping success");
